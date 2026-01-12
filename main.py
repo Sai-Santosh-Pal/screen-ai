28501121c98e89ba66b3946903153fe05c5679cf
Sai-Santosh-Pal <saisantoshpal2010@gmail.com>
2026-01-12 15:26:56 +0530

    working on screenshot and asking logics

--- main.py
+++ main.py
@@ -1,56 +1,49 @@
-# from openrouter import OpenRouter
 from dotenv import load_dotenv
 import os
+import requests
+import base64
+import mss
+import datetime
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
-
-# client = OpenRouter(
-#     api_key=api_key, 
-#     server_url="https://ai.hackclub.com/proxy/v1"
-# )
-
-prompt = str(input("Prompt: "))
-
-# response = client.chat.send(
-#     model="openai/gpt-5.1",
-#     messages=[
-#         {"role":"user","content":f"{asked}"}
-#     ],
-#     stream=False
-# )
-
-# print(response.choices[0].message.content)
-
-import requests
-import base64
 
 def encode_img(path):
     with open(path, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
-img=input("image: ")
-image_data = encode_img(img)
+def ask(img, prompt):
+    image_data = encode_img(img)
+    response = requests.post(
+        "https://ai.hackclub.com/proxy/v1/chat/completions",
+        headers={
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json"
+        },
+        json={
+            "model": "openai/gpt-5.1", # or google/gemini-2.5-flash
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": f"{prompt}"},
+                        {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image_data}"}}
+                    ]
+                }
+            ]
+        }
+    )
+    result = response.json()
+    return result["choices"][0]["message"]["content"]
 
-response = requests.post(
-    "https://ai.hackclub.com/proxy/v1/chat/completions",
-    headers={
-        "Authorization": f"Bearer {api_key}",
-        "Content-Type": "application/json"
-    },
-    json={
-        "model": "openai/gpt-5.1", # or google/gemini-2.5-flash
-        "messages": [
-            {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": f"{prompt}"},
-                    {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image_data}"}}
-                ]
-            }
-        ]
-    }
-)
+def take_ss():
+    path = datetime.datetime.now()
+    with mss.mss() as sct:
+        sct.shot(output=path.strftime("%H-%M-%S-%d-%m-%Y"))
+    return path.strftime("%H-%M-%S-%d-%m-%Y")
 
-result = response.json()
-print(result["choices"][0]["message"]["content"])
+print(take_ss())
+
+def get_next_task(goal):
+    current_pos = take_ss()
+    return ask(current_pos, goal)
